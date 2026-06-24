7741885f559ad393b64bcf5e38b8a1335cea41b1
versatilecommon <versatile.common128@passinbox.com>
2026-06-24 17:42:12 +0200

    first commit

---
+++ ImgProcessingBL.Test/ImgFiltersTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using _ImgProcessingBL;
+using Xunit.Abstractions;
+
+namespace ImgProcessingBL.Test
+{
+    public class ImgFiltersTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ImgFiltersTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void GetPixels_ReturnCorrectColor()
+        {
+            // Arrange
+            var image = new Bitmap(2, 2);
+            image.SetPixel(0, 0, Color.Gray);
+            image.SetPixel(0, 1, Color.Lime);
+
+            // Acts
+            byte[] result = {0x80, 0x80, 0x80, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00,0x00, 0x00};
+            byte[] pixelArray = ImgFilters.GetPixels(image);
+            _testOutputHelper.WriteLine(ImgFilters.GetPixels(image).ToString());
+            // Assert
+            Assert.Equal(pixelArray, result);
+        }
+    }
+}
---
+++ _ImgProcessingBL/ImgFilters.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace _ImgProcessingBL
+{
+    public static class ImgFilters
+    {
+        public static byte[] GetPixels(Bitmap bitmap)
+        {
+         
+            byte[] pixels = new byte[bitmap.Width * bitmap.Height * 3];
+            int index = 0;
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    
+                    pixels[index++] = pixelColor.R;
+                    pixels[index++] = pixelColor.G;
+                    pixels[index++] = pixelColor.B;
+                }
+            }
+
+            return pixels;
+        }
+    }
+}
