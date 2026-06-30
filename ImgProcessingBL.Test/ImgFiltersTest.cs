59c8c7ef76791a2d9fbad069871d35b3be66a0b3
versatilecommon <versatile.common128@passinbox.com>
2026-06-30 08:31:39 +0200

    migrate

--- ImgProcessingBL.Test/ImgFiltersTest.cs
+++ ImgProcessingBL.Test/ImgFiltersTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using Aspose.Drawing;
 using _ImgProcessingBL;
 using Xunit.Abstractions;
 
@@ -25,8 +25,58 @@ namespace ImgProcessingBL.Test
             byte[] result = {0x80, 0x80, 0x80, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00,0x00, 0x00};
             byte[] pixelArray = ImgFilters.GetPixels(image);
             _testOutputHelper.WriteLine(ImgFilters.GetPixels(image).ToString());
-            // Assert
+            // Assertbyte[] result = {0x80, 0x80, 0x80, 0x00, 0xFF, 0x00, 0x00, 0x00, 0
             Assert.Equal(pixelArray, result);
         }
+        [Fact]
+        public void GetBitmap_ReturnCorrectBitmap()
+        {
+            var expectedImage = new Bitmap(2, 2);
+            for (int y = 0; y < 2; y++)
+            {
+                for (int x = 0; x < 2; x++)
+                {
+                    expectedImage.SetPixel(x, y, Color.FromArgb(255, 0, 0, 0)); 
+                }
+            }
+            
+            expectedImage.SetPixel(0, 0, Color.FromArgb(255, 128, 128, 128));
+            
+            byte[] result = [128, 128, 128,  0,   0,   0,   0,   0,   0,   0,   0,   0];
+    
+            
+            var actualBitmap = ImgFilters.GetBitmap(result, 2, 2);
+    
+            
+            Assert.Equal(expectedImage.Width, actualBitmap.Width);
+            Assert.Equal(expectedImage.Height, actualBitmap.Height);
+    
+           
+            for (int y = 0; y < expectedImage.Height; y++)
+            {
+                for (int x = 0; x < expectedImage.Width; x++)
+                {
+                    int expectedToArgb = expectedImage.GetPixel(x, y).ToArgb();
+                    int actualToArgb = actualBitmap.GetPixel(x, y).ToArgb();
+            
+                    Assert.Equal(expectedToArgb, actualToArgb);
+                }
+            }
+        }
+        
+        [Fact]
+        public void ConvertToGray_ReturnCorrectBitmap()
+        {
+            byte[] inputPixels = [0xFF, 0x00, 0x00,  0,   0,   0,   0,   0,   0,   0,   0,   0];
+    
+        
+            byte[] actualPixels = ImgFilters.GetConvertToGray(inputPixels);
+    
+          
+            byte[] expectedPixels = [76, 76, 76,  0,   0,   0,   0,   0,   0,   0,   0,   0];
+   
+            Assert.Equal(expectedPixels, actualPixels);
+        }
+
     }
 }
--- _ImgProcessingBL/ImgFilters.cs
+++ _ImgProcessingBL/ImgFilters.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using Aspose.Drawing;
 
 namespace _ImgProcessingBL
 {
@@ -24,5 +24,45 @@ namespace _ImgProcessingBL
 
             return pixels;
         }
+
+        public static Bitmap GetBitmap(byte[] pixels, int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            int index = 0;
+    
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    bitmap.SetPixel(x, y, Color.FromArgb(pixels[index++], pixels[index++], pixels[index++]));
+                }
+            }
+            return bitmap;
+        }
+
+
+        public static byte[] GetConvertToGray(byte[] pixels)
+        {
+            // Step by 3 because each pixel contains 3 bytes (R, G, B)
+            for (int i = 0; i < pixels.Length; i += 3)
+            {
+                byte r = pixels[i];
+                byte g = pixels[i + 1];
+                byte b = pixels[i + 2];
+
+                // Apply grayscale formula using floating-point math
+                byte gray = (byte)(0.299 * r + 0.587 * g + 0.114 * b);
+
+                // Assign the calculated gray value to R, G, and B components
+                pixels[i] = gray;
+                pixels[i + 1] = gray;
+                pixels[i + 2] = gray;
+            }
+    
+            return pixels;
+        }
+
+        
+        
     }
 }
